3036059a245b30e9d99016a67285e919429204d0
Emre Eker <emeke21@student.sdu.dk>
2024-02-19 15:25:42 +0100

    small changes and pictures is now button

--- gui/app.py
+++ gui/app.py
@@ -1,77 +1,65 @@
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
-from pathlib import Path
 
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"assets/frame1")
+#global variables
+globalbackground = "#FFFFFF"
+borderthinkness = 5
 
-def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
-
-
+#creating the window
 window = Tk()
-
 window.geometry("1440x1024")
-window.configure(bg = "#FFFFFF")
+window.configure(bg = globalbackground)
+window.title("UMLPlanter")
 
+#creating and configuring the canvas
 canvas1 = Canvas(
     window,
-    bg = "#FFFFFF",
+    bg = globalbackground,
     height = 480,
     width = 250,
-    bd = 0,
-    highlightthickness = 5,
+    highlightthickness = borderthinkness,
     relief = "ridge"
 )
-
 canvas2 = Canvas(
     window,
-    bg = "#FFFFFF",
+    bg = globalbackground,
     height = 544,
     width = 250,
-    bd = 0,
-    highlightthickness = 5,
+    highlightthickness = borderthinkness,
     relief = "ridge"
 )
-
 canvas3 = Canvas(
     window,
-    bg = "#FFFFFF",
+    bg = globalbackground,
     height = 1024,
     width = 740,
-    bd = 0,
-    highlightthickness = 5,
+    highlightthickness = borderthinkness,
     relief = "ridge"
 )
-
 canvas4 = Canvas(
     window,
-    bg = "#FFFFFF",
+    bg = globalbackground,
     height = 1024,
     width = 450,
-    bd = 0,
-    highlightthickness = 5,
+    highlightthickness = borderthinkness,
     relief = "ridge"
 )
 
+#putting the canvas on the window
 canvas1.place(x = 0, y = 0)
 canvas2.place(x = 0, y = 480)
 canvas3.place(x = 250, y = 0)
 canvas4.place(x = 990, y = 0)
 
-image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-canvas1.create_image(
-    121.0,
-    115.0,
-    image=image_1
-)
+#buttom 1
+image_1 = PhotoImage(file="gui/assets/frame1/image_1.png")
+b = Button(canvas1, image=image_1, command=window.quit, bg = globalbackground)
+b.place(x=40, y=30)
 
-image_2 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-canvas1.create_image(
-    122.0,
-    272.99999999999994,
-    image=image_2
-)
+#buttom 2
+image_2 = PhotoImage(file="gui/assets/frame1/image_2.png")
+b2 = Button(canvas1, image=image_2, command=window.quit, bg = globalbackground)
+b2.place(x=40, y=250)
+
+#start the application
 window.resizable(False, False)
 window.mainloop()
