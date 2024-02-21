d9ff8ccfdd60f7a13adcddfacc2ceb4657b1b41f
Emre Eker <emeke21@student.sdu.dk>
2024-02-21 13:37:11 +0100

    place a box and move it around

--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from tkinter import Frame, Tk, Canvas, Entry, Text, Button, PhotoImage
+from canvas import canvas
 
 #global variables
 globalbackground = "#FFFFFF"
@@ -11,7 +12,7 @@ window.configure(bg = globalbackground)
 window.title("UMLPlanter")
 
 #creating and configuring the canvas
-canvas1 = Canvas(
+frame1 = Frame(
     window,
     bg = globalbackground,
     height = 480,
@@ -19,7 +20,7 @@ canvas1 = Canvas(
     highlightthickness = borderthinkness,
     relief = "ridge"
 )
-canvas2 = Canvas(
+frame2 = Frame(
     window,
     bg = globalbackground,
     height = 544,
@@ -27,15 +28,9 @@ canvas2 = Canvas(
     highlightthickness = borderthinkness,
     relief = "ridge"
 )
-canvas3 = Canvas(
-    window,
-    bg = globalbackground,
-    height = 1024,
-    width = 740,
-    highlightthickness = borderthinkness,
-    relief = "ridge"
-)
-canvas4 = Canvas(
+canvas = canvas(window, globalbackground, borderthinkness)
+
+frame4 = Frame(
     window,
     bg = globalbackground,
     height = 1024,
@@ -45,19 +40,30 @@ canvas4 = Canvas(
 )
 
 #putting the canvas on the window
-canvas1.place(x = 0, y = 0)
-canvas2.place(x = 0, y = 480)
-canvas3.place(x = 250, y = 0)
-canvas4.place(x = 990, y = 0)
+frame1.place(x = 0, y = 0)
+frame2.place(x = 0, y = 480)
+canvas.place(x = 250, y = 0)
+frame4.place(x = 990, y = 0)
+
+#creating a box on canvas3
+def place_box():
+    #creating the box
+    canvas.create_rectangle(
+        100, 100, 200, 200,
+        fill = "white",
+        outline = "black",
+        width = 2,
+        tags = "move")
+
 
 #buttom 1
 image_1 = PhotoImage(file="gui/assets/image_1.png")
-b = Button(canvas1, image=image_1, command=window.quit, bg = globalbackground)
+b = Button(frame1, image=image_1, command=place_box, bg = globalbackground)
 b.place(x=40, y=30)
 
 #buttom 2
 image_2 = PhotoImage(file="gui/assets/image_2.png")
-b2 = Button(canvas1, image=image_2, command=window.quit, bg = globalbackground)
+b2 = Button(frame1, image=image_2, command=window.quit, bg = globalbackground)
 b2.place(x=40, y=250)
 
 #start the application
---
+++ gui/canvas.py
@@ -0,0 +1,44 @@
+from tkinter import Canvas
+
+class canvas(Canvas):
+    def __init__(self, window, globalbackground, borderthinkness):
+        super().__init__(
+            window,
+            bg = globalbackground,
+            height = 1024,
+            width = 740,
+            highlightthickness = borderthinkness,
+            relief = "ridge",
+
+        )
+        self.place(x = 250, y = 0)
+
+        self._drag_data = {"x": 0, "y": 0, "item": None}
+        self.tag_bind("move", "<ButtonPress-1>", self.drag_start)
+        self.tag_bind("move", "<ButtonRelease-1>", self.drag_stop)
+        self.tag_bind("move", "<B1-Motion>", self.drag)
+
+    def drag_start(self, event):
+        """Begining drag of an object"""
+        # record the item and its location
+        self._drag_data["item"] = self.find_closest(event.x, event.y)[0]
+        self._drag_data["x"] = event.x
+        self._drag_data["y"] = event.y
+
+    def drag_stop(self, event):
+        """End drag of an object"""
+        # reset the drag information
+        self._drag_data["item"] = None
+        self._drag_data["x"] = 0
+        self._drag_data["y"] = 0
+
+    def drag(self, event):
+        """Handle dragging of an object"""
+        # compute how much the mouse has moved
+        delta_x = event.x - self._drag_data["x"]
+        delta_y = event.y - self._drag_data["y"]
+        # move the object the appropriate amount
+        self.move(self._drag_data["item"], delta_x, delta_y)
+        # record the new position
+        self._drag_data["x"] = event.x
+        self._drag_data["y"] = event.y
